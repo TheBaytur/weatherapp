d1e256cabbf77a650b5181629866926ab71c057a
TheBaytur <bayturcodding@gmail.com>
2025-05-30 16:09:44 +0800

    image

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,37 @@ class MyApp extends StatelessWidget {
           
           backgroundColor: Colors.white,
           leading: IconButton(icon: Icon(Icons.menu), 
-          onPressed: () {},
-          
+          onPressed: () {},   
           ),
           iconTheme: IconThemeData(color: Colors.black87),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
         ),
-        body: null,
+        body: _buildBody(),
       ),
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget> [
+        _headerImage(),
+      ],
+    ),
+  );
+}
+
+Image _headerImage () {
+  return Image(
+    image: NetworkImage('https://media.istockphoto.com/id/503801863/photo/rainbow-over-countryside.jpg?s=1024x1024&w=is&k=20&c=yAUrTQ8vg-CGz7pW1d7MlYcqKs-kHd313lUUUslWY48='),
+    fit: BoxFit.cover,
+    width: double.infinity,
+    height: 200.0,
+  );
 }
