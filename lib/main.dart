0dfcc7fb0451d8396c15d3193b1b823e205e824f
TheBaytur <bayturcodding@gmail.com>
2025-05-30 15:53:49 +0800

    Icon added

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      
+      
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Weather Application'),
-          centerTitle: true,
+          title: Text(
+            'Weather Application', 
+          style: TextStyle(
+            color: Colors.black87),
+            ),
+            centerTitle: true,
+          
+          backgroundColor: Colors.white,
+          leading: IconButton(icon: Icon(Icons.menu), 
+          onPressed: () {},
+          
+          ),
+          iconTheme: IconThemeData(color: Colors.black87),
         ),
         body: null,
       ),
