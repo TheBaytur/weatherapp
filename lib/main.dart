9e5fa50834ca7b45f86280f84d0d7295fa6419c4
TheBaytur <bayturcodding@gmail.com>
2025-06-02 20:24:47 +0800

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,44 @@ class BodyListView extends StatelessWidget {
 }
 
 Widget _myListView() {
-  final List<String> items = List<String>.generate(10000, (index) => 'Item $index');
+  final List<ListItem> items = List<ListItem>.generate(
+    10000, 
+  (i) => i % 6 == 0 
+      ? HeadingItem('Heading $i')
+      : MessageItem('Message $i', 'Message content for this $i'));
+
   return ListView.builder(
     itemCount: items.length,
     itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text('${items[index]}'),
-              subtitle: Text('Subtitle for ${items[index]}'),
-              leading: Icon(Icons.cloud, color: Colors.blue),
-              trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
-            ),
-          );
-
+          // return Card(
+          //   child: ListTile(
+          //     title: Text('${items[index]}'),
+          //     subtitle: Text('Subtitle for ${items[index]}'),
+          //     leading: Icon(Icons.cloud, color: Colors.blue),
+          //     trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+          //   ),
+          // );
     },
-    
-    
   );
 }
+
+  abstract class ListItem {}
+
+
+
+class HeadingItem implements ListItem {
+  final String heading;
+  HeadingItem(this.heading);
+
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+}
+
 
 
 
